6f4bf0004d0cbdd747bd61111cf088f934d31b07
mozix5 <mosinmd827@gmail.com>
2023-11-19 12:05:04 +0530

    updated the font size, color

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -5,19 +5,20 @@ const Posts = (props) => {
   return (
     <div className="py-8  border-b px-1 md:px-2">
       <div className="flex justify-between items-center">
-        <div className=" font-bold capitalize text-xl">{props.title}</div>
+        <div className=" font-bold capitalize text-xl sm:text-2xl">{props.title}</div>
         <button className=" rounded-full bg-yellow-400 p-1">
           <LuThumbsUp className="text-white" />
         </button>
       </div>
-      <div className="py-8  text-[#808080]">{props.content}</div>
-      <div className="flex justify-between text-xs py-2  text-[#808080]">
-        <div className="flex gap-1 font-semibold items-center">
+      <div className="py-8 sm:text-xl text-[#808080]">{props.content}</div>
+      <div className="flex justify-between text-sm py-2  text-[#808080]">
+        <div className="flex gap-1 font-semibold items-center ">
           <div className=" text-blue-400">{props.type}</div>
           <div>by {props.userName}</div>
         </div>
-        <div className="flex gap-1 text-[#808080]">
+        <div className="flex gap-1 sm:gap-2 items-center">
           <div>{props.createdAt}</div>
+          <span className=" h-1 w-1 bg-[#808080]  rounded-full"/>
           <div>{props.readAt} Read</div>
           <div>{props.views} Views</div>
         </div>
